0502c370d4fd8a4aaa21f646a6b69e87cc7420d5
ihesah1 <Hesah.Shaman.elq.js@tuwaiq.edu.sa>
2021-12-06 11:00:35 +0300

    solve exam

---
+++ backend_debugging/todoback/db/models/todo.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+  task: { type: Boolean, required: true },
+  isDel: { type: Boolean, default: false },
+  isCompleted: { type: Boolean, default: false },
+  timeStamp: { type: Date, default: new Date() },
+});
+
+module.exports = mongoose.model("Todo", todoSchema);
